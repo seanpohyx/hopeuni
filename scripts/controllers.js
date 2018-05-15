84c820d9da1aca440a7b6ab77f78df62aa0abe76
jeremiah-ang <jeremiahang.developer@gmail.com>
2018-05-16 00:00:25 +0800

    api + schedule

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,5 +1,9 @@
 var app = angular.module("hopeUNIapp", [])
 
+/*
+ * User info controller
+ * load from api and populate front end
+ */
 app.controller("userInfoCtrl", 
 	($scope, $http) => {
 
@@ -10,7 +14,7 @@ app.controller("userInfoCtrl",
 		}
 
 		$scope.display = (name) => {
-			return get_user_sheet($http, name, sheet_api)
+			return get_user_info($http, name, sheet_api)
 			.then ((value) => {
 				$scope.selecteduser = value
 				$scope.$apply()
@@ -26,6 +30,10 @@ app.controller("userInfoCtrl",
 		})
 	})
 
+/*
+ * Announcement controller
+ * load from api and populate front end
+ */
 app.controller("announcementCtrl", 
 	($scope, $http) => {
 		get_announcement($http, sheet_api)
@@ -33,4 +41,43 @@ app.controller("announcementCtrl",
 			$scope.annos = Array.prototype.map.call(annos, (anno) => anno[0])
 		})
 	})
+
+
+/*
+ * Scheduler Controller
+ * fetch from api and start countdown
+ */
+ app.controller("scheduleCtrl", ($scope, $http, $interval) => {
+ 	const pad_time = (time) => (time < 10) ? "0" + time : time
+ 	get_schedule($http, sheet_api)
+ 	.then((data) => {
+ 		let schedule = data['schedules']
+ 		const heads = data['headers']
+ 		const first_event = schedule.shift()
+ 		const starttime = parseInt(first_event["start time"])
+ 		let hour = parseInt(starttime / 100)
+ 		let min = starttime % 100
+ 		let secs = 0
+
+ 		
+ 		countdown = $interval(() => {
+ 			$scope.hrs = pad_time(hour)
+	 		$scope.min = pad_time(min)
+	 		$scope.secs = pad_time(secs)
+
+	 		secs = (secs-- < 1) ? 59 : secs
+	 		if (hour == 0 && min == 0 && secs == 0) {
+	 			$scope.secs = pad_time(secs)
+	 			return stop_interval()
+	 		} 
+	 		min = (secs < 59) ? min : (min-- < 1) ? 59 : min
+	 		hour = (secs < 59 || min < 59) ? hour : (hour == 0) ? hour : hour - 1
+
+	 		
+ 		}, 100)
+ 		let stop_interval = () => { console.log("stop!"); $interval.cancel(countdown)}
+ 		$scope.heads = heads
+ 		$scope.nexts = schedule
+ 	})
+ })
 			
--- scripts/googlesheet.js
+++ scripts/googlesheet.js
@@ -1,54 +1,133 @@
-var data = {};
+var sheet_cache = {}
+var data_cache = {}
 
+// Cache sheet data, to prevent too many api calls
+function is_cached_sheet (sheetname) {
+	return (Object.prototype.hasOwnProperty.call(sheet_cache, sheetname))
+}
+function cache_sheet (data, sheetname) {
+	sheet_cache[sheetname] = data 
+}
+function get_cached_sheet (sheetname) {
+	return sheet_cache[sheetname]
+}
+
+// Cache processed sheet data, to prevent duplicate processing
+function cache_processed_sheet (data, processedname) {
+	data_cache[processedname] = data
+}
+function is_cached_processed_sheet (processedname) {
+	return (Object.prototype.hasOwnProperty.call(data_cache, processedname))
+}
+function get_cached_processed_sheet (processedname) {
+	return data_cache[processedname]
+}
+
+
+/*
+ * Call google sheet api to get data from sheetid/sheetname
+ * Returns a promise, that resolves the fetched data 
+ */
 function load_sheet($http, api_obj, sheetname) {
-	if (Object.prototype.hasOwnProperty.call(data, sheetname)) {
+	if (is_cached_sheet(sheetname)) {
 		console.log ("Cached: " + sheetname)
-		return Promise.resolve(data[sheetname])
+		return Promise.resolve(get_cached_sheet(sheetname))
 	}
-	console.log("Fetching Data...")
+
+	// Fetch sheet data
 	const sheet_id = api_obj.SHEET_ID 
 	const api_key = api_obj.API_KEY
 	const url = "https://sheets.googleapis.com/v4/spreadsheets/"+sheet_id+"/values/"+sheetname+"?key="+api_key
 	return $http.get(url)
-	.then((res) => {
-		console.log(res)
-		data[sheetname] = res
-		return Promise.resolve(res)
-	})
+		.then((res) => {
+			cache_sheet(res, sheetname)
+			return Promise.resolve(res)
+		})
 }
 
+/*
+ * Loads user sheet data and 
+ * process the sheet data
+ */
 function get_users($http, api_obj) {
-	return load_sheet($http, api_obj, "users")
+	const sheetname = "users"
+
+	if (is_cached_processed_sheet(sheetname)) {
+		return Promise.resolve(get_cached_processed_sheet(sheetname))
+	}
+
+	return load_sheet($http, api_obj, sheetname)
 	.then((res) => {
-		values = res.data.values
-		headers = values[0]
+
+		// Process user sheet data 
+		const values = res.data.values
+		const headers = values[0]
 		let data = {}
-		for (var i = 1; i < values.length; i++) {
-			var user = {}
-			for (var j = 0; j < headers.length; j++) {
+		for (let i = 1; i < values.length; i++) {
+			let user = {}
+			for (let j = 0; j < headers.length; j++) {
 				user[headers[j]] = values[i][j]
 			}
 			data[values[i][0]] = user
 		}
-		console.log("END Fetching Data...")
-		return Promise.resolve({
+		const processed_sheet = {
 			"headers": headers,
 			"users": data
-		})
+		}
+		cache_processed_sheet(processed_sheet, sheetname)
+		return Promise.resolve(processed_sheet)
 	})
 
 }
 
-function get_user_sheet($http, name, api_obj) {
+/*
+ * Get info of a user
+ * the person name should be a key in dictionary
+ */
+function get_user_info($http, name, api_obj) {
 	return get_users($http, api_obj)
 			.then((data) => {
 				return Promise.resolve(data["users"][name])
 			})
 }
 
+/*
+ * Get announcement from annoucements sheet
+ */
 function get_announcement($http, api_obj) {
 	return load_sheet($http, api_obj, "announcements")
 	.then((res) => {
 		return Promise.resolve(res.data.values)
+	})
+}
+
+/*
+ * Get schedule from schedule sheet and
+ * processed it into an object
+ */
+function get_schedule($http, api_obj) {
+	sheetname = "schedule"
+	if (is_cached_processed_sheet(sheetname)) {
+		return Promise.resolve(get_cached_processed_sheet(sheetname))
+	}
+
+	return load_sheet($http, api_obj, sheetname)
+	.then((res) => {
+		let data = res.data.values
+		let headers = data[0]
+		let schedules = []
+		for (var i = 1; i < data.length; i++) {
+			let schedule = {}
+			for (var j = 0; j < headers.length; j++) {
+				schedule[headers[j]] = data[i][j]
+			}
+			schedules.push(schedule)
+		}
+		let processed_data = {
+			headers,
+			schedules
+		}
+		cache_processed_sheet(processed_data, sheetname)
+		return Promise.resolve(processed_data)
 	})
 }
